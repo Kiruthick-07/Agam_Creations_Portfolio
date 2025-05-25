bf0f7080ed95e08184ce195738191ee13b9c0d39
Kiruthick-07 <tamilelectroniv@gmail.com>
2025-05-25 21:48:29 +0530

    project section commit

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var nav = document.querySelector(".side-navbar")
+var bgcol = document.querySelector(".body")
 function opennav(){
     nav.style.display="block"
 }
 function closenav(){
     nav.style.display="none"
+}
+function bg_col(){
+    bgcol.style.background = 'white';
 }
